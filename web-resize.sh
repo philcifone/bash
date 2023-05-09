a9b4945935ce6a2f192c281540bc619d1d413c0a
philcifone <philcif@gmail.com>
2023-05-09 03:12:35 -0400

    add top level user input question for blog

--- web-resize.sh
+++ web-resize.sh
@@ -13,11 +13,14 @@ magick mogrify -resize 1200x1200 *.jpg &&
 # format all JPEG in directory to webp
 magick mogrify -format webp -quality 90 *.jpg
 
+# ask user if for photos or blog
+read -p "Is this for photos or blog? " top_level
+
 # ask user to designate path (should exist already)
 read -p "what directory is this? " img_folder
 
 # set variable for img_path, include user input to designate filename
-img_path="/images/photos/${img_folder}/"
+img_path="/images/${top_level}/${img_folder}/"
 
 # list all webp files and save to file
 ls *.webp >> img_list.txt 
