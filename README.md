f6202e1f6a4a54fe5ac2925476374030ec4398b4
philcifone <philcif@gmail.com>
2023-05-23 03:10:45 -0400

    add backup-script.sh and updated README

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ web-resize.sh
 - Resizes JPEG images in a directory to 1200px x 1200px and converts to webp
 - Outputs directory list to text file
 - Outputs links formatted in markdown to local files for website.
+
+backup-script.sh
+    
+    Thanks to Jay from Learn Linux TV for the lesson in BASH scripting for this script. It has been modified to fit my needs.
---
+++ backup-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Thanks Jay from Learn Linux TV for the BASH scripting tutorial!
+#
+# This has been modified to fit my needs.
+#
+#
+
+# check to make sure user entered two arguments
+if [ $# -ne 2 ]
+then
+    echo "usage: backup-script.sh <source> <dest>"
+    echo "please try again." 
+    exit 1
+fi
+
+# check if rsync is installed
+if ! command -v rsync > /dev/null 2>&1
+then
+    echo "this script requires rsync."
+    echo "consult your package manager"
+    exit 2
+fi
+
+# capture current date
+current_date=$(date +%Y-%m-%d)
+        
+        # set rsync flags and options
+	rsync_options="-avPb --backup-dir $2/$current_date --delete"
+
+$(which rsync) $rsync_options $1 $2/current >> backup_$current_date.log
