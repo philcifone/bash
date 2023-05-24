c40b7fbda34732c0bc775b0018b69266fb0c6794
Phil Cifone <118232939+philcifone@users.noreply.github.com>
2023-05-24 03:26:23 -0400

    nevermind

--- main-backup.sh
+++ main-backup.sh
@@ -34,20 +34,15 @@ rsync_sync_opt="-av --delete"
 
 # run rsync backup command and save output to log
 echo "syncing scratch disk katahdin to gamehenge"
-$(which rsync) $rsync_sync_opt $(katahdin)Uploads/2023/ $(gamehenge)Photography/ALL\ UPLOADS/2023/
->> $(log_path)backup_$current_date.log
-$(which rsync) $rsync_sync_opt $(katahdin)Lightroom/ $(gamehenge)Photography/Lightroom/NEW_Lightroom_Backups/
->> $(log_path)backup_$current_date.log
-$(which rsync) $rsync_sync_opt $(katahdin)PhotosLibrary $(gamehenge)Photography/
->> $(log_path)backup_$current_date.log
+$(which rsync) $rsync_sync_opt $(katahdin)Uploads/2023/ $(gamehenge)Photography/ALL\ UPLOADS/2023/ >> $(log_path)backup_$current_date.log
+$(which rsync) $rsync_sync_opt $(katahdin)Lightroom/ $(gamehenge)Photography/Lightroom/NEW_Lightroom_Backups/ >> $(log_path)backup_$current_date.log
+$(which rsync) $rsync_sync_opt $(katahdin)PhotosLibrary $(gamehenge)Photography/ >> $(log_path)backup_$current_date.log
 
 echo "syncing volume gamehenge to catalyst"
-$(which rsync) $rsync_sync_opt --exclude=Movies_Shows $(gamehenge) $(catalyst)
->> $(log_path)backup_$current_date.log
+$(which rsync) $rsync_sync_opt --exclude=Movies_Shows $(gamehenge) $(catalyst) >> $(log_path)backup_$current_date.log
 
 echo "Backing up gamehenge to abraxas"
-rsync-backup.sh $rsync_bkup_opt --exclude=TOM $(gamehenge) $(abraxas) 
->> $(log_path)backup_$current_date.log
+rsync-backup.sh $rsync_bkup_opt --exclude=TOM $(gamehenge) $(abraxas) >> $(log_path)backup_$current_date.log
 
 # Print end status message.
 echo "Backup finished"
