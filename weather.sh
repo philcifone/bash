bf2c9a079740a291e5461b13cd50367f65f22fce
philcifone <phil@philcifone.com>
2023-10-18 12:59:11 -0400

    added evening weather fetch

--- weather.sh
+++ weather.sh
@@ -10,13 +10,19 @@ weather_data=$(curl -s "$weather_api")
 # Check if the request was successful
 if [ $? -eq 0 ]; then
     # Parse and display relevant weather information using jq
-    echo "Weather Forecast for Your Location:"
+    echo "Weather Forecast for Your Location Today:"
     echo "-------------------------------------"
     echo "Location: $(echo "$weather_data" | jq -r '.properties.periods[0].name') in Drexel Hill, PA"
     echo "Temperature: $(echo "$weather_data" | jq -r '.properties.periods[0].temperature')°F"
     echo "Conditions: $(echo "$weather_data" | jq -r '.properties.periods[0].shortForecast')"
     echo "Humidity: $(echo "$weather_data" | jq -r '.properties.periods[0].relativeHumidity.value')%"
     echo "Wind: $(echo "$weather_data" | jq -r '.properties.periods[0].windSpeed')"
+    echo "-------------------------------------"
+    echo "Location: $(echo "$weather_data" | jq -r '.properties.periods[1].name') in Drexel Hill, PA"
+    echo "Temperature: $(echo "$weather_data" | jq -r '.properties.periods[1].temperature')°F"
+    echo "Conditions: $(echo "$weather_data" | jq -r '.properties.periods[1].shortForecast')"
+    echo "Humidity: $(echo "$weather_data" | jq -r '.properties.periods[1].relativeHumidity.value')%"
+    echo "Wind: $(echo "$weather_data" | jq -r '.properties.periods[1].windSpeed')"
 else
     echo "Failed to fetch weather data. Check your internet connection or the API URL."
 fi
