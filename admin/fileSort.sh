8f2fe06fff8cf03c260738c52311c3b41b4dc579
philcifone <phil@philcifone.com>
2023-10-28 11:48:51 -0400

    updated fileSort

--- admin/fileSort.sh
+++ admin/fileSort.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
-#
-#
 
-# User input for directory selection
-# 
 echo "What directory do you want to sort?"
-
 read sortPath
 
-cd $sortPath
+if [ ! -d "$sortPath" ]; then
+  echo "Directory '$sortPath' does not exist."
+  exit 1
+fi
+
+cd "$sortPath"
 
 # Create subdirectories in lowercase if they don't exist
-sudo mkdir -p documents images movies audio rss compressed
+for dir in documents images movies audio rss compressed; do
+  if [ ! -d "$dir" ]; then
+    mkdir -p "$dir"
+  fi
+done
+
+# Function to move files to the appropriate directory
+move_files() {
+  local source_dir="$1"
+  local target_dir="$2"
+  shift 2
+  for ext in "$@"; do
+    mv -t "$target_dir" *.$ext 2>/dev/null
+    mv -t "$target_dir" *.$(tr '[:upper:]' '[:lower:]' <<< "$ext") 2>/dev/null
+  done
+}
 
 # Move documents to the "documents" directory
-sudo mv *.pdf *.PDF *.docx *.xlsx *.pptx *.txt documents/
+move_files "$sortPath" "documents" pdf PDF docx xlsx pptx txt
 
 # Move images to the "images" directory
-sudo mv *.jpg *.JPG *.jpeg *.JPEG *.png *.gif *.bmp *.webp images/
+move_files "$sortPath" "images" jpg JPG jpeg JPEG png gif bmp webp
 
 # Move movie files to the "movies" directory
-sudo mv *.mp4 *.avi *.mkv *.mov movies/
+move_files "$sortPath" "movies" mp4 avi mkv mov
 
 # Move audio files to the "audio" directory
-sudo mv *.mp3 *.wav *.flac audio/
+move_files "$sortPath" "audio" mp3 wav flac
 
 # Move rss files to the "rss" directory
-sudo mv *.rss rss/
+move_files "$sortPath" "rss" rss
 
 # Move compressed files to the "compressed" directory
-sudo mv *.tar.* *.tar *.zip
+move_files "$sortPath" "compressed" tar gz zip ZIP
 
 echo "Files organized by media type."
-
-# Function to create a dated folder and move files into it
-#move_to_dated_folder() {
-#  for file in "$1"/*; do
-#    [ -f "$file" ] && mkdir -p "$1/$(date -r "$file" '+%Y-%m-%d')" && mv "$file" "$1/$(date -r "$file" '+%Y-%m-%d')/"
-#  done
-#}
-
-# Move files into dated folders based on their creation date
-#move_to_dated_folder documents
-#move_to_dated_folder images
-#move_to_dated_folder movies
-#move_to_dated_folder audio
-
-#echo "Files organized by creation date."
-
